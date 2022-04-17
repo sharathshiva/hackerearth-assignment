c25fd6e0c88b5d8c592513147cf5e50e92f9593e
sharath <48129125+sharathshiva@users.noreply.github.com>
2022-04-17 20:01:05 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hacker-earth-assignment';
   mesage = 'Added new message variable';
+  message2 = 'Added Message2 variabe';
 }
